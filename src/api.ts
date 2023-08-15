cf0b719f73a5433f63fe9a76228b1b084ccc01bb
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2023-08-15 10:10:39 -0300

    login: verificação da senha

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 const conta = {
-    email: 'nath@dio.bank',
+    email: 'alex@dio.bank',
     password: '123456',
-    name: 'Nathaly Souza',
+    name: 'Alexandre Garcia',
     balance: 2000.00,
     id: '1'
 }
