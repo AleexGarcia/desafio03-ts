2f96e07b8a8bc3cb3fd889a1e85cc6bced346407
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2023-08-15 10:13:13 -0300

    testes login

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -4,13 +4,21 @@ describe('login', () => {
 
     const mockEmail = 'alex@dio.bank'
     const mockSenha = '123456'
-    it('Deve exibir um alert com boas vindas caso o email seja válido', async() => {
+    it('Deve exibir um alert com boas vindas caso o email e senha sejam válidos', async() => {
         const response = await login(mockEmail, mockSenha);
         expect(response).toBeTruthy()
     })
 
     it('Deve exibir um erro caso o email seja inválido', async() => {
         const response = await login('email@invalido.com','senhaInvalida')
+        expect(response).toBeFalsy()
+    })
+    it('Deve exibir um erro caso o email seja válido e senha incorreta', async() => {
+        const response = await login(mockEmail,'senhaInvalida')
+        expect(response).toBeFalsy()
+    })
+    it('Deve exibir um erro caso o email seja inválido e senha incorreta', async() => {
+        const response = await login('email@invalido.com',mockSenha)
         expect(response).toBeFalsy()
     })
 })
